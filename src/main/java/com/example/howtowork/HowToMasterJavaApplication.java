70dae095d0d5fe2266f5c5f8de8eeb3058b2a395
LongVo248 <vohoanglongfx@gmail.com>
2021-12-18 20:04:43 +0700

    spring-boot-1-helloworld-@Component-@Autowired

---
+++ src/main/java/com/example/howtowork/HowToMasterJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.howtowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToMasterJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToMasterJavaApplication.class, args);
+    }
+
+}
